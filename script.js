16961b1f526f2cca99a78ccbdd2af45bfccb5316
sagar kumar mathuri <sagarku2002@gmail.com>
2026-04-05 23:35:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+// function secondHighest(arr) {
+//   if (arr.length < 2) return -Infinity;
+
+//   let highest = -Infinity;
+//   let second = -Infinity;
+
+//   for (let num of arr) {
+//     if (num > highest) {
+//       second = highest;
+//       highest = num;
+//     } else if (num > second && num < highest) {
+//       second = num;
+//     }
+//   }
+
+//   return second;
+// }
 function secondHighest(arr) {
   if (arr.length < 2) return -Infinity;
 
-  let highest = -Infinity;
+  let max = -Infinity;
   let second = -Infinity;
 
   for (let num of arr) {
-    if (num > highest) {
-      second = highest;
-      highest = num;
-    } else if (num > second && num < highest) {
+    if (num > max) {
+      second = max;
+      max = num;
+    } else if (num > second && num < max) {
       second = num;
     }
   }
